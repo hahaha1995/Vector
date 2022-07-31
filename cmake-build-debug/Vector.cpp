985a2a722943f81fcd6252c2a278defc17a1ef80
xiao <berumottoX@163.com>
2022-07-31 13:04:29 +0800

    Initial commit

---
+++ cmake-build-debug/Vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 杨潇 on 2022/7/31.
+//
+
+#include "Vector.h"
---
+++ cmake-build-debug/Vector.h
@@ -0,0 +1,15 @@
+//
+// Created by 杨潇 on 2022/7/31.
+//
+
+#ifndef VECTOR_VECTOR_H
+#define VECTOR_VECTOR_H
+
+template <typename T>
+class Vector {
+public:
+
+
+};
+
+#endif //VECTOR_VECTOR_H
